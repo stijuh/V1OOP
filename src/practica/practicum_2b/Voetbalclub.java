3034e0f7d171a52cce30ea06fe2fd3c73c5c66da
stijuh <70705964+stijuh@users.noreply.github.com>
2021-02-06 20:14:32 +0100

    Practicum 2a toegevoegd

---
+++ src/practica/practicum_2b/Voetbalclub.java
@@ -0,0 +1,37 @@
+package practica.practicum_2b;
+
+public class Voetbalclub {
+    private String naam;
+    private int aantalGewonnen;
+    private int aantalGelijk;
+    private int aantalVerloren;
+    private int punten;
+
+    public Voetbalclub(String nm) {
+        naam = nm;
+    }
+
+    public void verwerkResultaat(char ch) {
+        if (ch == 'w')
+            aantalGewonnen = aantalGewonnen + 1;
+        if (ch == 'g')
+            aantalGelijk = aantalGelijk + 1;
+        if (ch == 'v')
+            aantalVerloren = aantalVerloren + 1;
+    }
+
+    public int aantalPunten() {
+        punten = (aantalGewonnen * 3) + aantalGelijk;
+        return punten;
+    }
+
+    public int aantalGespeeld() {
+        return (aantalGewonnen + aantalGelijk + aantalVerloren);
+    }
+
+    public String toString() {
+        String str = naam + "\t" + aantalGespeeld() + aantalGewonnen + aantalGelijk + aantalVerloren + aantalPunten();
+        return str;
+    }
+
+}
